6127bce9e5c1179e2d8544e314f496d35e097775
yjdwbj <yjdwbj@gmail.com>
2020-11-24 13:10:05 +0800

    添加经度处理

--- traceroute_geojson.sh
+++ traceroute_geojson.sh
@@ -64,39 +64,48 @@ function _trace_route(){
         _value=$(jq -r ".report.hubs[$k]" $RFILE)
         _asn=$(jq -r ".ASN" <<< "$_value")
         _host=$(jq -r ".host" <<< "$_value")
-        if [ "$_asn" == "AS???" ];then
+        # ASN查不到，且地址是IPv4内网地址。
+        if [[ "$_asn" == "AS???" && ($_host =~ ^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.) || "$_host" == "xxx")]];then
             echo "局部路由节： $_host"
             continue
         fi 
         if [ "$_host" != "xxx" ]; then
-
-            # 如果中间有空格，表示有两段，FQDN域名 (ip地址)
+            # 如果_host中间有空格，表示有两段，FQDN域名 (ip地址)
             if [[ $_host == *\ * ]]; then
               _host=$(echo $_host | awk '{print $2}' | sed  's/[()]//g') 
+            fi
+            # 如果_host不是IPv4,也不是IPv6,那就是一个域名。
+            if [[ $_host == *[g-z\-]* ]]; then
+                # 在现在这种IPv4和IPV6混用的情况下,使用AAAA的选项查询主机的IPv6 AAAA记录
+                _host=$(dig $_host AAAA +short)
             fi
             case $GEOIP_LOOKUP in
                 local)
                     # 合并两行为一行：sed  'N;s/\n/ /'
                     # sed 除去一些如：<dobule> <utf8_string>,再去掉最后一个豆号，它就是json内容了。
-                    DB_RES==$(mmdblookup --file $MMDB --ip $_host location)
+                    DB_RES=$(mmdblookup --file $MMDB --ip $_host location)
                     GEOTEXT=$(echo $DB_RES | sed 's/<[0-9a-z_]*>/,/g' | tr -d '\n' | sed -E  's/(.*),/\1/')
                     ;;
                 network)
                     # 把lon换成了longitude,lat换成了latitude,as换成了ASN.
-                    GEOTEXT=$(curl -s $IP_API/$_host | jq '. + {longitude :.lon} | . + {latitude :.lat} | . + {ASN:.as} | del(.lat,.lon,.as)')
+                    GEOTEXT=$(curl -s $IP_API/$_host | jq '. + {longitude :.lon} | . + {latitude :.lat} | . + {time_zone:.timezone } |. + {ASN:.as} | del(.lat,.lon,.as,.timezone)')
                     ;;
             esac
-        else
-           GEOTEXT='{"accuracy_radius": 0,"latitude": 0,"longitude": 0,"time_zone": "null"}'
-
         fi
+
+       
         # 把新的kv加入对应的数组对像中去。
+
         TMP=$(echo $JSON_DATA | jq ".hubs[$k] |= . + $GEOTEXT")
         # 替换变量里的内容。
         JSON_DATA=$TMP
     done
     # 这里再把修改后变量内容重定向到一个文件。
-    echo $JSON_DATA | jq '.'
+    echo $JSON_DATA | jq '.' | tee $_host.json
+     # 除去longitude为空的结点，把负西经改成正数处理。但是在边界值画线时，平面与球还是会有
+    GEOTEXT=$(echo $JSON_DATA |jq '[.hubs[] | select(.longitude!=null)] | map(.longitude = if .longitude < 0 then 180+(180 + .longitude) else .longitude end)')
+    # GEOTEXT=$(echo $JSON_DATA |jq '[.hubs[] | select(.longitude!=null)]')
+    JSON_DATA=$(echo $JSON_DATA | jq ".hubs = $GEOTEXT" )
 
 }
 
@@ -122,18 +131,18 @@ function _create_geojson(){
 
     GEOJSON=$(echo '{"type":"FeatureCollection","features": [] }' | jq '.' )
     # 生成线段信息。
-    _coordinates=$(echo $JSON_DATA | jq '[.hubs[] | select(.longitude!=null)] | map([.longitude,.latitude])')
+    _coordinates=$(echo $JSON_DATA | jq '.hubs | map([.longitude,.latitude])')
     LINEARR=$(echo $JSON_DATA | jq '.| {"type":"Feature","geometry":{"type":"LineString","coordinates":null},"properties":{"name":.mtr.dst, "id":.mtr.tests}}')
     LINEARR=$(echo $LINEARR | jq ".geometry.coordinates = $_coordinates")
     # 清除[null,null],再清除[]
     # LINEARR=$(echo $LINES | jq  'del(.geometry.coordinates[][] | nulls)' | jq 'del(.geometry.coordinates[] | select(length==0))')
     # 生成坐标信息。
-    TMP=$(echo $JSON_DATA | jq '[.hubs[] | select(.longitude !=null)]')
-    POINTS=$(echo $TMP | jq 'map({"type":"Feature","geometry":{"type":"Point","coordinates":[.longitude,.latitude]},"properties":{"name":.host, "id":.count,"timezone":.timezone,"ASN":.ASN}})')
+    POINTS=$(echo $JSON_DATA | jq '.hubs | map({"type":"Feature","geometry":{"type":"Point","coordinates":[.longitude,.latitude]},"properties":{"name":.host, "id":.count,"timezone": .time_zone,"ASN":.ASN}})')
     GEOJSON=$(echo $GEOJSON | jq ".features |= . + [$LINEARR,$POINTS[]]")
     # 转换成网络请求的百分比数据格式,uri_escape。
     URI_DATA=$(echo $GEOJSON | jq -sRr @uri)
     echo $GEOJSON | jq '.' > $GFILE
+    rm $RFILE
     # 打开网页提交数据。
     $OPEN $DOMAIN/#data=data:application/json,$URI_DATA
 }
@@ -157,7 +166,7 @@ HOSTIP='1.1.1.1'
 GFILE="geojson_$HOSTIP.json"
 GEOIP_LOOKUP='local' # network
 COUNT=10
-MTR_ARGS='-zbj'
+MTR_ARGS='-y 2 -nzbj'
 
 
 
